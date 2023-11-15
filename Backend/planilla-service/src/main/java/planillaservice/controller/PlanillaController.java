105d5650d666b72f9721b086ec511f691c24561d
Gonzalo Ibáñez <gonzalo.ibanez@usach.cl>
2023-11-15 13:47:50 -0300

    Cambio en la carpeta de config data

--- Backend/planilla-service/src/main/java/planillaservice/controller/PlanillaController.java
+++ Backend/planilla-service/src/main/java/planillaservice/controller/PlanillaController.java
@@ -1,7 +1,35 @@
 package planillaservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import planillaservice.service.SubirPrueba;
 
 @RestController
+@RequestMapping("/planilla")
 public class PlanillaController {
+
+	@Autowired
+	SubirPrueba subirPrueba;
+
+	@GetMapping
+	public String mainSubir() {
+		return "subir";
+	}
+	@PostMapping("/examen")
+	public String uploadExamen(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+		subirPrueba.guardar(file);
+		boolean correcto = subirPrueba.leerCsvExamen(file.getOriginalFilename());
+		if (correcto)
+		{
+			redirectAttributes.addFlashAttribute("mensaje", "¡Archivo cargado correctamente!");
+		}
+		else
+		{
+			redirectAttributes.addFlashAttribute("mensaje", "Verifique el archivo que esta subiendo");
+		}
+		return "redirect:/"; //Cambiar
+	}
 }
